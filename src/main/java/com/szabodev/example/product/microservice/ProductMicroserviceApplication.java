24f8babbc220228d37bc8e7b80884698f273bebd
Zoltán Szabó <szaboz89@gmail.com>
2019-11-06 07:22:50 +0100

    Init

---
+++ src/main/java/com/szabodev/example/product/microservice/ProductMicroserviceApplication.java
@@ -0,0 +1,12 @@
+package com.szabodev.example.product.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductMicroserviceApplication.class, args);
+    }
+}
